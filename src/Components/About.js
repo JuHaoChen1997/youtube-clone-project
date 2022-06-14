0e1aee18b6ec60c81a909e400f80d43ac22fa779
lily08 <liliangelxd@gmail.com>
2022-06-14 17:18:10 -0400

    mock up about page

---
+++ src/Components/About.js
@@ -0,0 +1,239 @@
+function About() {
+  return (
+    <div>
+      <section>
+        <p>project Description</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu
+          felis sit amet felis tristique pharetra. Vivamus eu egestas magna.
+          Suspendisse ut lacus felis. Sed sed ex vitae urna mollis consectetur
+          vel non velit. Donec in eleifend tortor, at lacinia arcu. Ut dapibus
+          semper sem eu rhoncus. Pellentesque venenatis vestibulum feugiat.
+          Maecenas a pretium turpis. Morbi lacinia nulla nec bibendum interdum.
+          Aenean vitae mauris eget felis interdum aliquam in ac nulla. Ut
+          pretium elit libero, et vehicula leo porta non. Pellentesque varius
+          sapien sed vestibulum tincidunt. Etiam euismod odio at nulla feugiat,
+          eu scelerisque velit malesuada. Vestibulum euismod, elit et pretium
+          mattis, lectus lectus sollicitudin nibh, at pretium tortor tellus id
+          erat. Quisque efficitur faucibus nunc vel accumsan. Vivamus ut
+          pulvinar nunc. Phasellus non ante pharetra, luctus ipsum ullamcorper,
+          dictum lorem. Donec eu faucibus elit. Donec eu commodo leo, nec
+          vulputate lacus. Praesent vitae lorem ut ligula hendrerit efficitur et
+          vel augue. Ut non nulla a felis porttitor mattis. Praesent rhoncus
+          aliquet facilisis. Sed placerat vel neque at posuere. Nam placerat
+          pellentesque elit, at facilisis libero venenatis facilisis. Vivamus
+          ligula nibh, sollicitudin sed tincidunt a, blandit sit amet tellus.
+          Nam eu nulla molestie, viverra tortor id, accumsan lorem. Aliquam
+          pellentesque euismod felis, a malesuada urna vulputate euismod. Nam
+          vestibulum tincidunt ante, sit amet ultrices velit mattis egestas.
+          Vivamus dapibus porta nulla, tristique cursus erat elementum non.
+          Donec et dictum lectus, in tincidunt leo. Praesent eget nisl sit amet
+          magna bibendum congue at et quam. Praesent tellus dui, tristique at
+          risus ut, dictum rutrum massa. Pellentesque placerat ultrices urna, eu
+          dapibus nibh molestie non. Praesent non posuere est, sit amet
+          elementum lorem. Duis bibendum elementum leo, quis commodo libero
+          aliquam sed. Mauris maximus vel sapien ac rutrum. Ut auctor libero
+          sem, ac elementum nibh cursus a. Aliquam nibh arcu, posuere vitae
+          lacinia sit amet, bibendum eu diam. Aliquam vitae libero sed metus
+          pulvinar ornare. Nunc commodo rhoncus posuere. Aenean in volutpat
+          lectus. Pellentesque sit amet quam non augue elementum consectetur non
+          id eros. Quisque consectetur lacus iaculis nisl dictum hendrerit.
+          Quisque vestibulum metus vitae nulla varius gravida. Curabitur laoreet
+          ornare libero quis sodales. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. In commodo turpis
+          sed erat pulvinar, sit amet rhoncus arcu rutrum. Proin eget dolor
+          justo. Integer in ex ac tortor vestibulum consequat. Sed facilisis
+          nisi vitae lorem ullamcorper, quis tempus mi euismod. Nullam accumsan
+          placerat dui a vehicula. Maecenas eget nunc quam. Suspendisse id justo
+          nec magna euismod faucibus id eu nisl. Suspendisse hendrerit suscipit
+          ornare. Nunc sed odio interdum, ornare ligula eget, sollicitudin
+          mauris. Etiam convallis rutrum lacus nec dapibus. Etiam interdum, quam
+          a hendrerit fringilla, mi elit tristique lacus, a tristique diam nisl
+          at erat. Aliquam erat volutpat. Cras pellentesque mi massa. Nam
+          imperdiet gravida diam, quis sagittis felis viverra ac. Nunc ornare
+          enim bibendum, pellentesque felis vitae, vehicula felis. Ut a rutrum
+          erat. Etiam elit nulla, volutpat sit amet urna et, tincidunt imperdiet
+          urna. Pellentesque nec ex vestibulum turpis consectetur euismod.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Donec eget dui ut augue bibendum auctor.
+        </p>
+      </section>
+      <article>
+        <p>juhao</p>
+        <a href={'https://github.com/JuHaoChen1997'}>GitHub</a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu
+          felis sit amet felis tristique pharetra. Vivamus eu egestas magna.
+          Suspendisse ut lacus felis. Sed sed ex vitae urna mollis consectetur
+          vel non velit. Donec in eleifend tortor, at lacinia arcu. Ut dapibus
+          semper sem eu rhoncus. Pellentesque venenatis vestibulum feugiat.
+          Maecenas a pretium turpis. Morbi lacinia nulla nec bibendum interdum.
+          Aenean vitae mauris eget felis interdum aliquam in ac nulla. Ut
+          pretium elit libero, et vehicula leo porta non. Pellentesque varius
+          sapien sed vestibulum tincidunt. Etiam euismod odio at nulla feugiat,
+          eu scelerisque velit malesuada. Vestibulum euismod, elit et pretium
+          mattis, lectus lectus sollicitudin nibh, at pretium tortor tellus id
+          erat. Quisque efficitur faucibus nunc vel accumsan. Vivamus ut
+          pulvinar nunc. Phasellus non ante pharetra, luctus ipsum ullamcorper,
+          dictum lorem. Donec eu faucibus elit. Donec eu commodo leo, nec
+          vulputate lacus. Praesent vitae lorem ut ligula hendrerit efficitur et
+          vel augue. Ut non nulla a felis porttitor mattis. Praesent rhoncus
+          aliquet facilisis. Sed placerat vel neque at posuere. Nam placerat
+          pellentesque elit, at facilisis libero venenatis facilisis. Vivamus
+          ligula nibh, sollicitudin sed tincidunt a, blandit sit amet tellus.
+          Nam eu nulla molestie, viverra tortor id, accumsan lorem. Aliquam
+          pellentesque euismod felis, a malesuada urna vulputate euismod. Nam
+          vestibulum tincidunt ante, sit amet ultrices velit mattis egestas.
+          Vivamus dapibus porta nulla, tristique cursus erat elementum non.
+          Donec et dictum lectus, in tincidunt leo. Praesent eget nisl sit amet
+          magna bibendum congue at et quam. Praesent tellus dui, tristique at
+          risus ut, dictum rutrum massa. Pellentesque placerat ultrices urna, eu
+          dapibus nibh molestie non. Praesent non posuere est, sit amet
+          elementum lorem. Duis bibendum elementum leo, quis commodo libero
+          aliquam sed. Mauris maximus vel sapien ac rutrum. Ut auctor libero
+          sem, ac elementum nibh cursus a. Aliquam nibh arcu, posuere vitae
+          lacinia sit amet, bibendum eu diam. Aliquam vitae libero sed metus
+          pulvinar ornare. Nunc commodo rhoncus posuere. Aenean in volutpat
+          lectus. Pellentesque sit amet quam non augue elementum consectetur non
+          id eros. Quisque consectetur lacus iaculis nisl dictum hendrerit.
+          Quisque vestibulum metus vitae nulla varius gravida. Curabitur laoreet
+          ornare libero quis sodales. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. In commodo turpis
+          sed erat pulvinar, sit amet rhoncus arcu rutrum. Proin eget dolor
+          justo. Integer in ex ac tortor vestibulum consequat. Sed facilisis
+          nisi vitae lorem ullamcorper, quis tempus mi euismod. Nullam accumsan
+          placerat dui a vehicula. Maecenas eget nunc quam. Suspendisse id justo
+          nec magna euismod faucibus id eu nisl. Suspendisse hendrerit suscipit
+          ornare. Nunc sed odio interdum, ornare ligula eget, sollicitudin
+          mauris. Etiam convallis rutrum lacus nec dapibus. Etiam interdum, quam
+          a hendrerit fringilla, mi elit tristique lacus, a tristique diam nisl
+          at erat. Aliquam erat volutpat. Cras pellentesque mi massa. Nam
+          imperdiet gravida diam, quis sagittis felis viverra ac. Nunc ornare
+          enim bibendum, pellentesque felis vitae, vehicula felis. Ut a rutrum
+          erat. Etiam elit nulla, volutpat sit amet urna et, tincidunt imperdiet
+          urna. Pellentesque nec ex vestibulum turpis consectetur euismod.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Donec eget dui ut augue bibendum auctor.
+        </p>
+      </article>
+      <article>
+        <p>herman</p>
+        <a href={' https://github.com/HC-Guerrero'}>GitHub</a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu
+          felis sit amet felis tristique pharetra. Vivamus eu egestas magna.
+          Suspendisse ut lacus felis. Sed sed ex vitae urna mollis consectetur
+          vel non velit. Donec in eleifend tortor, at lacinia arcu. Ut dapibus
+          semper sem eu rhoncus. Pellentesque venenatis vestibulum feugiat.
+          Maecenas a pretium turpis. Morbi lacinia nulla nec bibendum interdum.
+          Aenean vitae mauris eget felis interdum aliquam in ac nulla. Ut
+          pretium elit libero, et vehicula leo porta non. Pellentesque varius
+          sapien sed vestibulum tincidunt. Etiam euismod odio at nulla feugiat,
+          eu scelerisque velit malesuada. Vestibulum euismod, elit et pretium
+          mattis, lectus lectus sollicitudin nibh, at pretium tortor tellus id
+          erat. Quisque efficitur faucibus nunc vel accumsan. Vivamus ut
+          pulvinar nunc. Phasellus non ante pharetra, luctus ipsum ullamcorper,
+          dictum lorem. Donec eu faucibus elit. Donec eu commodo leo, nec
+          vulputate lacus. Praesent vitae lorem ut ligula hendrerit efficitur et
+          vel augue. Ut non nulla a felis porttitor mattis. Praesent rhoncus
+          aliquet facilisis. Sed placerat vel neque at posuere. Nam placerat
+          pellentesque elit, at facilisis libero venenatis facilisis. Vivamus
+          ligula nibh, sollicitudin sed tincidunt a, blandit sit amet tellus.
+          Nam eu nulla molestie, viverra tortor id, accumsan lorem. Aliquam
+          pellentesque euismod felis, a malesuada urna vulputate euismod. Nam
+          vestibulum tincidunt ante, sit amet ultrices velit mattis egestas.
+          Vivamus dapibus porta nulla, tristique cursus erat elementum non.
+          Donec et dictum lectus, in tincidunt leo. Praesent eget nisl sit amet
+          magna bibendum congue at et quam. Praesent tellus dui, tristique at
+          risus ut, dictum rutrum massa. Pellentesque placerat ultrices urna, eu
+          dapibus nibh molestie non. Praesent non posuere est, sit amet
+          elementum lorem. Duis bibendum elementum leo, quis commodo libero
+          aliquam sed. Mauris maximus vel sapien ac rutrum. Ut auctor libero
+          sem, ac elementum nibh cursus a. Aliquam nibh arcu, posuere vitae
+          lacinia sit amet, bibendum eu diam. Aliquam vitae libero sed metus
+          pulvinar ornare. Nunc commodo rhoncus posuere. Aenean in volutpat
+          lectus. Pellentesque sit amet quam non augue elementum consectetur non
+          id eros. Quisque consectetur lacus iaculis nisl dictum hendrerit.
+          Quisque vestibulum metus vitae nulla varius gravida. Curabitur laoreet
+          ornare libero quis sodales. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. In commodo turpis
+          sed erat pulvinar, sit amet rhoncus arcu rutrum. Proin eget dolor
+          justo. Integer in ex ac tortor vestibulum consequat. Sed facilisis
+          nisi vitae lorem ullamcorper, quis tempus mi euismod. Nullam accumsan
+          placerat dui a vehicula. Maecenas eget nunc quam. Suspendisse id justo
+          nec magna euismod faucibus id eu nisl. Suspendisse hendrerit suscipit
+          ornare. Nunc sed odio interdum, ornare ligula eget, sollicitudin
+          mauris. Etiam convallis rutrum lacus nec dapibus. Etiam interdum, quam
+          a hendrerit fringilla, mi elit tristique lacus, a tristique diam nisl
+          at erat. Aliquam erat volutpat. Cras pellentesque mi massa. Nam
+          imperdiet gravida diam, quis sagittis felis viverra ac. Nunc ornare
+          enim bibendum, pellentesque felis vitae, vehicula felis. Ut a rutrum
+          erat. Etiam elit nulla, volutpat sit amet urna et, tincidunt imperdiet
+          urna. Pellentesque nec ex vestibulum turpis consectetur euismod.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Donec eget dui ut augue bibendum auctor.
+        </p>
+      </article>
+      <article>
+        <p>lily</p>
+        <a href={'https://github.com/liliwu8'}>GitHub</a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu
+          felis sit amet felis tristique pharetra. Vivamus eu egestas magna.
+          Suspendisse ut lacus felis. Sed sed ex vitae urna mollis consectetur
+          vel non velit. Donec in eleifend tortor, at lacinia arcu. Ut dapibus
+          semper sem eu rhoncus. Pellentesque venenatis vestibulum feugiat.
+          Maecenas a pretium turpis. Morbi lacinia nulla nec bibendum interdum.
+          Aenean vitae mauris eget felis interdum aliquam in ac nulla. Ut
+          pretium elit libero, et vehicula leo porta non. Pellentesque varius
+          sapien sed vestibulum tincidunt. Etiam euismod odio at nulla feugiat,
+          eu scelerisque velit malesuada. Vestibulum euismod, elit et pretium
+          mattis, lectus lectus sollicitudin nibh, at pretium tortor tellus id
+          erat. Quisque efficitur faucibus nunc vel accumsan. Vivamus ut
+          pulvinar nunc. Phasellus non ante pharetra, luctus ipsum ullamcorper,
+          dictum lorem. Donec eu faucibus elit. Donec eu commodo leo, nec
+          vulputate lacus. Praesent vitae lorem ut ligula hendrerit efficitur et
+          vel augue. Ut non nulla a felis porttitor mattis. Praesent rhoncus
+          aliquet facilisis. Sed placerat vel neque at posuere. Nam placerat
+          pellentesque elit, at facilisis libero venenatis facilisis. Vivamus
+          ligula nibh, sollicitudin sed tincidunt a, blandit sit amet tellus.
+          Nam eu nulla molestie, viverra tortor id, accumsan lorem. Aliquam
+          pellentesque euismod felis, a malesuada urna vulputate euismod. Nam
+          vestibulum tincidunt ante, sit amet ultrices velit mattis egestas.
+          Vivamus dapibus porta nulla, tristique cursus erat elementum non.
+          Donec et dictum lectus, in tincidunt leo. Praesent eget nisl sit amet
+          magna bibendum congue at et quam. Praesent tellus dui, tristique at
+          risus ut, dictum rutrum massa. Pellentesque placerat ultrices urna, eu
+          dapibus nibh molestie non. Praesent non posuere est, sit amet
+          elementum lorem. Duis bibendum elementum leo, quis commodo libero
+          aliquam sed. Mauris maximus vel sapien ac rutrum. Ut auctor libero
+          sem, ac elementum nibh cursus a. Aliquam nibh arcu, posuere vitae
+          lacinia sit amet, bibendum eu diam. Aliquam vitae libero sed metus
+          pulvinar ornare. Nunc commodo rhoncus posuere. Aenean in volutpat
+          lectus. Pellentesque sit amet quam non augue elementum consectetur non
+          id eros. Quisque consectetur lacus iaculis nisl dictum hendrerit.
+          Quisque vestibulum metus vitae nulla varius gravida. Curabitur laoreet
+          ornare libero quis sodales. Class aptent taciti sociosqu ad litora
+          torquent per conubia nostra, per inceptos himenaeos. In commodo turpis
+          sed erat pulvinar, sit amet rhoncus arcu rutrum. Proin eget dolor
+          justo. Integer in ex ac tortor vestibulum consequat. Sed facilisis
+          nisi vitae lorem ullamcorper, quis tempus mi euismod. Nullam accumsan
+          placerat dui a vehicula. Maecenas eget nunc quam. Suspendisse id justo
+          nec magna euismod faucibus id eu nisl. Suspendisse hendrerit suscipit
+          ornare. Nunc sed odio interdum, ornare ligula eget, sollicitudin
+          mauris. Etiam convallis rutrum lacus nec dapibus. Etiam interdum, quam
+          a hendrerit fringilla, mi elit tristique lacus, a tristique diam nisl
+          at erat. Aliquam erat volutpat. Cras pellentesque mi massa. Nam
+          imperdiet gravida diam, quis sagittis felis viverra ac. Nunc ornare
+          enim bibendum, pellentesque felis vitae, vehicula felis. Ut a rutrum
+          erat. Etiam elit nulla, volutpat sit amet urna et, tincidunt imperdiet
+          urna. Pellentesque nec ex vestibulum turpis consectetur euismod.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Donec eget dui ut augue bibendum auctor.
+        </p>
+      </article>
+    </div>
+  )
+}
+
+export default About
