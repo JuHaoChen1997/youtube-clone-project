0a2ac336d614e92ff953fb5b107874302cb7b8f1
lily08 <liliangelxd@gmail.com>
2022-06-14 22:33:52 -0400

    forms created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css'
 import React from 'react'
-import About from './Components/About'
+import About from './components/About'
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      userName: '',
+      userComment: '',
+      comments: [],
+    }
   }
+
+  handleNameChange = (event) => {
+    const { value } = event.target
+    this.setState({ userName: value })
+  }
+
+  handleCommentChange = (event) => {
+    const { value } = event.target
+    this.setState({ userComment: value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const { userName, userComment, comments } = this.state
+
+    this.setState({
+      comments: [...comments, (<strong>{userName}</strong>), userComment],
+    })
+  }
+
   render() {
+    const { userComment, userName, comments } = this.state
+    console.log(userName)
     return (
       <div className='App'>
         <main className='main'>
@@ -17,6 +43,34 @@ class App extends React.Component {
             <h2 className='about-button'>About</h2>
           </div>
           <About />
+          <form onSubmit={this.handleSubmit}>
+            Name
+            <br />
+            <input
+              id='text'
+              name='name'
+              type='text'
+              value={userName}
+              onChange={this.handleNameChange}
+            />
+            <br />
+            <br />
+            Comments
+            <br />
+            <input
+              id='text'
+              name='text'
+              type='text'
+              value={userComment}
+              onChange={this.handleCommentChange}
+            />
+            <br />
+            <br />
+            <button>Submit</button>
+            {comments.map((comment, ind) => (
+              <p key={ind}>{comment}<br/></p>
+            ))}
+          </form>
         </main>
       </div>
     )
