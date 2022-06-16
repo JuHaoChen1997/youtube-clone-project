fb5c107b7f9e352963387a611692de696339e3c3
JuHaoChen1997 <juhaochen97@gmail.com>
2022-06-16 11:51:56 -0400

    add the modal feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,53 +1,61 @@
-import React from 'react'
-import './SearchBar.css'
-// import Modal from './Modal'
+import React from "react";
+import "./SearchBar.css";
+import Modal from "./Modal";
 
 class SearchBar extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      searchInput: '',
-      // show: false,
-    }
+      searchInput: "",
+      show: false,
+    };
   }
 
   updateSearchInputHandler = (event) => {
     this.setState({
       searchInput: event.target.value,
-    })
-  }
-  // showModal = () => {
-  //   this.setState({ show: true })
-  // }
+    });
+  };
 
-  // closeButton = () => {
-  //   this.setState({ show: false })
-  // }
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  closeButton = () => {
+    this.setState({ show: false });
+  };
+
+  checkSearchInput = () => {
+    return this.state.searchInput.length >= 20;
+  };
+
   render() {
-    const { fetchRequestHandler } = this.props
+    const { fetchRequestHandler } = this.props;
 
     return (
-      <section className='searchField'>
+      <section className="searchField">
         <input
-          className='textField'
-          type='text'
+          className="textField"
+          type="text"
           value={this.state.searchInput}
           onChange={this.updateSearchInputHandler}
         />
         <button
-          type='button'
-          className='searchButton'
+          type="button"
+          className="searchButton"
           onClick={() => {
-            fetchRequestHandler(this.state.searchInput)
+            this.checkSearchInput()
+              ? this.showModal()
+              : fetchRequestHandler(this.state.searchInput);
           }}
         >
           Search
         </button>
-        {/* <button onClick={() => this.showModal()}>Show Modal</button>
-        <Modal show={this.state.show} closeButton={this.closeButton} /> */}
+        {/* <button onClick={() => this.showModal()}>Show Modal</button> */}
+        <Modal show={this.state.show} closeButton={this.closeButton} />
       </section>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
