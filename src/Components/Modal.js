ba62f6c9f9a45516a8648d0b2df849654f174ab9
lily08 <liliangelxd@gmail.com>
2022-06-17 19:18:19 -0400

    change the x letter button bigger and font-size

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,17 +1,16 @@
-import './Modal.css';
+import './Modal.css'
 function Modal(props) {
   if (!props.show) {
-    return null;
+    return null
   }
 
   return (
-    <div className='modal'>
-      <div className='modal-content'>
+    <div className='modal' onClick={props.closeButton}>
+      <div className='modal-content' onClick={(e) => e.stopPropagation()}>
         <div className='modal-header'>
           <div className='button' onClick={props.closeButton}>
             X
           </div>
-
           <h4 className='modal-title'>ERROR!!!</h4>
         </div>
         <div className='modal-body'>
@@ -20,7 +19,7 @@ function Modal(props) {
         <div className='modal-footer'></div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Modal;
+export default Modal
