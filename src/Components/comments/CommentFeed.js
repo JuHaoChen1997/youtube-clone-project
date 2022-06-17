140479bd77e5491794a5fc146ad593f7e6004852
JuHaoChen1997 <juhaochen97@gmail.com>
2022-06-17 00:43:27 -0400

    add local storage to comment

---
+++ src/Components/comments/CommentFeed.js
@@ -0,0 +1,15 @@
+function CommentFeed(props) {
+  const { comments } = props;
+
+  const displayComments = comments.map((comment) => {
+    return (
+      <div>
+        <h4>UserName: {comment.userName}</h4>
+        <p>Comment: {comment.comment}</p>
+      </div>
+    );
+  });
+  return <section>{displayComments}</section>;
+}
+
+export default CommentFeed;
