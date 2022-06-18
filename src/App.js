e14eb8670b25ee42122b8f952e13d5f6d4617d87
JuHaoChen1997 <juhaochen97@gmail.com>
2022-06-18 15:41:31 -0400

    set api key as variable

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ class App extends React.Component {
     let youtubeVideos = [];
     if (searchInput !== "") {
       fetch(
-        // `https://youtube.googleapis.com/youtube/v3/search?maxResults=10&q=${searchInput}&key=${process.env.REACT_APP_API_KEY}&part=snippet`
-        `https://youtube.googleapis.com/youtube/v3/search?maxResults=10&q=${searchInput}&key=AIzaSyCpmUJbJ5kPdifR9m62nsOXYohK53HFlag&part=snippet`
+        `https://youtube.googleapis.com/youtube/v3/search?maxResults=10&q=${searchInput}&key=${process.env.REACT_APP_API_KEY}&part=snippet`
       )
         .then((result) => {
           return result.json();
