01485367d893e0623ddd947f18553486f832d70a
lily08 <liliangelxd@gmail.com>
2022-06-21 10:59:43 -0400

    saved about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,12 @@
 import './About.css'
 function About() {
   return (
-    <div>
+    <div className='about'>
       <section>
         <h1 className='ProjectDesc'>Project Description</h1>
-
-        <p className='Entry1'></p>
+        <p className='Entry1'>
+          This Youtube clone project created by Juhao, Hermanclyde, and Lily
+        </p>
       </section>
       <br />
       <br />
@@ -14,14 +15,14 @@ function About() {
           <p>Juhao Chen</p>
         </strong>
         <a className='Member1' href={'https://github.com/JuHaoChen1997'}>
-          <img
-            src={require('./GithubLogo/github-icon.png')}
-            alt='logo'
-            width={100}
-            height={100}
-          />
+          GitHub
         </a>
-        <p></p>
+        <p>
+          An aspiring software engineer of Pursuit's 8.3 cohort, I love to code
+          and solve challenging problems. I'm currently perfecting my skills and
+          am training to become a software engineer to solve real-world
+          problems. When I'm not coding, I enjoy drawing and listening to music.
+        </p>
       </article>
       <hr />
       <article className='herman-bio'>
@@ -29,14 +30,14 @@ function About() {
           <p>Hermanclyde Guerrero</p>
         </strong>
         <a className='Member2' href={' https://github.com/HC-Guerrero'}>
-          <img
-            src={require('./GithubLogo/github-icon.png')}
-            alt='logo'
-            width={100}
-            height={100}
-          />
+          GitHub
         </a>
-        <p></p>
+        <p>
+          An aspiring software engineer of Pursuit's 8.3 cohort, with a strong
+          passion to enter video game development. I dabble in video game
+          modifications and have a strong inclination towards history,
+          economics, and old school video games.
+        </p>
       </article>
       <hr />
       <article className='lily-bio'>
@@ -44,14 +45,13 @@ function About() {
           <p>Lily Wu</p>
         </strong>
         <a className='Member3' href={'https://github.com/liliwu8'}>
-          <img
-            src={require('./GithubLogo/github-icon.png')}
-            alt='logo'
-            width={100}
-            height={100}
-          />
+          GitHub
         </a>
-        <p></p>
+        <p>
+          I am a fullstack software engineer in NYC with a Bachelor of
+          Administrative in Marketing degree. I like to code and solve problems.
+          When I am not coding I like to eat.
+        </p>
       </article>
     </div>
   )
