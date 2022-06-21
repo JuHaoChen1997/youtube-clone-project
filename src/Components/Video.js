66ce5b50a3e7f844951fe72f73a5dfedcc1773e9
JuHaoChen1997 <juhaochen97@gmail.com>
2022-06-21 18:59:06 -0400

    add comment to Video

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Video.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Video.css";
 
 class Video extends React.Component {
   render() {
-    console.log(this.props);
     const { title, thumbnails, videoId } = this.props.data;
 
+    // Display videos on the video gallery
     return (
-      <div className='video'>
-        <Link className='a' to={`/videos/${videoId}`}>
-          <img className='thumbnails' src={thumbnails} alt={videoId} />
-          <p className='vidName'>{title}</p>
+      <div className="video">
+        <Link className="a" to={`/videos/${videoId}`}>
+          <img className="thumbnails" src={thumbnails} alt={videoId} />
+          <p className="vidName">{title}</p>
         </Link>
       </div>
     );
